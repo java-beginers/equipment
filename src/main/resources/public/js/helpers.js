ba02424f7876b44f599e37f0ad3164a5245a519b
voronin.leonid@otr.ru <voronin.leonid@otr.ru>
2017-09-14 23:48:07 +0700

    Добавлена библиотека jQuery. Пробуем добавлять эффекты.

--- src/main/resources/public/js/helpers.js
+++ src/main/resources/public/js/helpers.js
@@ -1,6 +1,16 @@
 // Выводит на экран окно подтверждения удаления записи
 function confirmDelete(itemName, deleteLink) {
-    if (true == confirm('Действительно удалить запись "' + itemName + '"?')) {
+    if (true === confirm('Действительно удалить запись "' + itemName + '"?')) {
         window.location.href = deleteLink;
     }
 }
+
+$(document).ready(function () {
+    $('table.dataTable tbody tr').hover(
+        function () {
+            $(this).find('td div.rowActions').fadeIn(200)
+        },
+        function () {
+            $(this).find('td div.rowActions').fadeOut(200)
+        });
+});

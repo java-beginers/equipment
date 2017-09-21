8f7a9b89784f67fd36e2b2ee8eee922d8f659d11
voronin.leonid@otr.ru <voronin.leonid@otr.ru>
2017-09-21 19:30:39 +0700

    Доработки
    * Дорабатываем форму редактирования оборудования.
    * Добавляем обраобтку исключений и логирование.

--- src/main/java/ru/web/equipment/controller/EquipmentController.java
+++ src/main/java/ru/web/equipment/controller/EquipmentController.java
@@ -52,12 +52,13 @@ public class EquipmentController {
     public String saveNewEquipment(@ModelAttribute Equipment equipment) {
 
         if (equipment != null) {
-            equipment.setCategory(fetchCategory(equipment.getCategoryCode()));
-            equipment.setVendor(fetchVendor(equipment.getVendorCode()));
-
-            // TODO: 20.09.17 Проверить данные на корректность.
-
-            equipmentsRepository.save(equipment);
+            try {
+                equipment.setCategory(fetchCategory(equipment.getCategoryCode()));
+                equipment.setVendor(fetchVendor(equipment.getVendorCode()));
+                equipmentsRepository.save(equipment);
+            } catch (Exception e) {
+                log.error("Ошибка при сохранении оборудования.", e);
+            }
         }
         return REDIRECT_TO_LIST;
     }
@@ -67,12 +68,21 @@ public class EquipmentController {
     public String editEquipment(Model model, @PathVariable long equipmentId) {
 
         if (equipmentId > 0) {
-            Equipment equipment = equipmentsRepository.findOne(equipmentId);
-            if (equipment != null) {
-                // Такой объект есть
-                model.addAttribute("equipment", equipment);
-                return "editEquipment";
+            try {
+                Equipment equipment = equipmentsRepository.findOne(equipmentId);
+                if (equipment != null) {
+                    Iterable<Category> categories = categoriesRepository.findAll();
+                    Iterable<Vendor> vendors = vendorsRepository.findAll();
+                    model.addAttribute("allCategories", categories);
+                    model.addAttribute("allVendors", vendors);
+                    model.addAttribute("equipment", equipment);
+                    return "editEquipment";
+                }
+            } catch (Exception e) {
+                log.error("ошика при попытке построить форму редактирования для оборудования с ID {}", equipmentId, e);
             }
+        } else {
+            log.error("Странное значение идентификатора оборудования {}", equipmentId);
         }
         return REDIRECT_TO_LIST;
     }
@@ -82,11 +92,18 @@ public class EquipmentController {
     public String deleteEquipment(@PathVariable long equipmentId) {
 
         if (equipmentId > 0) {
-            Equipment equipment = equipmentsRepository.findOne(equipmentId);
-            if (equipment != null) {
-                // Такой объект есть
-                equipmentsRepository.delete(equipment);
+            try {
+                Equipment equipment = equipmentsRepository.findOne(equipmentId);
+                if (equipment != null) {
+                    equipmentsRepository.delete(equipment);
+                } else {
+                    log.warn("Не удалось найти оборудование с ID {} для удаления.", equipmentId);
+                }
+            } catch (Exception e) {
+                log.error("Ошибка при удалении оборудования с ID {}", equipmentId, e);
             }
+        } else {
+            log.error("Странный идентификатор оборудования {}", equipmentId);
         }
         return REDIRECT_TO_LIST;
     }

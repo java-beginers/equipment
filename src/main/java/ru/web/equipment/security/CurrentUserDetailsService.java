ed00fd174313bddd42a507eb3fd098729f6f2242
voronin.leonid@otr.ru <voronin.leonid@otr.ru>
2017-10-09 21:14:04 +0700

    Вернули виртуальному администратору роль админа.

--- src/main/java/ru/web/equipment/security/CurrentUserDetailsService.java
+++ src/main/java/ru/web/equipment/security/CurrentUserDetailsService.java
@@ -57,7 +57,7 @@ public class  CurrentUserDetailsService implements UserDetailsService {
         }
         User user = new User();
         user.setFullName("Администратор проекта");
-        user.setRole(UserRole.ROLE_USER);
+        user.setRole(UserRole.ROLE_ADMIN);
         user.setLogin(login);
         user.setPasswordHash(passwordEncoder.encode(password));
         user.setEnabled(true);

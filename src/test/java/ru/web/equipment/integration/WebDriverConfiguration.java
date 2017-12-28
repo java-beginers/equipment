5e9d61987b0caf07f2a202f95e42eae218922c02
voronin.leonid@otr.ru <voronin.leonid@otr.ru>
2017-12-28 21:21:49 +0700

    Поддержка браузера в конфигурации.

--- src/test/java/ru/web/equipment/integration/WebDriverConfiguration.java
+++ src/test/java/ru/web/equipment/integration/WebDriverConfiguration.java
@@ -1,15 +1,23 @@
 package ru.web.equipment.integration;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class WebDriverConfiguration {
+    private static final String BROWSER_KEY = "browser";
     // Кодировщик пароля
     @Bean(name = "webDriver")
     public WebDriver getPasswordEncoder() {
-        return new FirefoxDriver();
+        String browser = System.getProperty(BROWSER_KEY);
+        switch (browser) {
+            case "chrome" : return new ChromeDriver();
+            case "opera" : return new OperaDriver();
+            default: return new FirefoxDriver();
+        }
     }
 }
